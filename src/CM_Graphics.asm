0896a6e66166d133a422c715911e98879bd08eda
Controllerhead <Controllerhead@nesblast.com>
2023-08-18 00:05:10 -0400

    Added graphics from Practice Rom in to CHR files
    pixely wixely scanalinotronirfic!

--- src/CM_Graphics.asm
+++ src/CM_Graphics.asm
@@ -1,3 +1,28 @@
+; Graphics Replacements
+
+.BANK $26 SLOT "$8000"
+
+.ORG $0000
+.INCBIN "chr\mapCoors.chr"
+
+.ORG $0800
+.INCBIN "chr\fontRiYAN.chr"
+
+; Oi one stray pixel are yeh? ya bloodie kitten me?!
+.ORG $1653
+.DB $00
+
+.ORG $1600
+.INCBIN "chr\hudIcons.chr"
+
+.ORG $1800
+.INCBIN "chr\mapHeader.chr"
+
+
 .BANK $27 SLOT "$8000"
+
+.ORG $0800
+.INCBIN "chr\icons.chr"
+
 .ORG $1000
-.INCBIN "src\gra_input.bin"
+.INCBIN "chr\input.chr"
